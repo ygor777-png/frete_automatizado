357df2abd29bf6e6d16d1c4a07bf2c6a2320213e
ygor777-png <ygor.souza1@outlook.com.br>
2025-11-18 16:22:12 -0300

    Fix input label for Valor FE in app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if pagina == "Gestão de Fretes":
         descarga = st.text_input("Local de descarga"
         produto = st.text_input("Produto")
         caminhao = st.text_input("Caminhão")
-        valor_fe = st.number_input ("Valor FE, min_value=0.0, step=10.0)
+        valor_fe = st.number_input ("Valor FE", min_value=0.0, step=10.0)
         valor_pj = st.number_input("Valor PJ", min_value=0.0, step=10.0)
         valor_pf = st.number_input("Valor PF", min_value=0.0, step=10.0)
         submit = st.form_submit_button("Adicionar")
